42347b3fdd47a05a41fd0675c638e46eb35b6745
Michael Cohen <macball2000@yahoo.com>
2023-11-01 16:27:36 -0400

    Products: Index Page Basic Setup

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import Link from 'next/link';
+import { products } from '../lib/products/inventory';
+import Image from 'next/image';
 
 export default function ProductsPage() {
+  
   return (
-    <div>ProductsPage</div>
+    <div className="flex flex-col items-center px-8">
+      <h1 className='text-2xl my-8'>Products</h1>
+      <div className='flex flex-wrap justify-center md:justify-start gap-4'>
+        {products.map(item => (
+        <Link 
+        href={'/products/' + item.id} 
+        className='flex flex-col items-center gap-3 p-4 bg-opacity-10 bg-gray-500' 
+        key={item.id}>
+          <Image
+            src={item.images[0]}
+            width={300}
+            height={300}
+            alt={item.name}
+          />
+          <p>{item.name}</p>
+          <p>${item.price}</p>
+        </Link>
+        ))}
+      </div>
+    </div>
   )
 }
