cc1ed3789444f01ac2271191e7825f4cc7156adb
Michael Cohen <macball2000@yahoo.com>
2023-11-02 14:43:28 -0400

    Navbar: Added Cart Modal + Styles

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,19 +1,19 @@
+import Cart from "@/components/Cart";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-center align-center px-5 py-5 sticky top-0 border-b border-white"><div className="xl:container flex flex-grow justify-between align-center">
+    <nav className="w-full flex justify-center align-center px-5 py-5 sticky top-0 border-b border-white bg-black bg-opacity-80 z-50 transition-all">
+        <div className="flex flex-grow justify-between align-center">
         <div className="flex items-center">
             <Link href={'/'} className="group flex justify-center align-center px-2 ">
                 <div className="group-hover:border-b border-white">Logo</div>
             </Link>
             <Link href={'/products'} className="group flex justify-center align-center px-2 "><div className="group-hover:border-b border-white">Products</div></Link>
         </div>
-        <div className="p-2 border rounded-md cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-            </svg>
-        </div></div>
+        <Cart/>
+        
+        </div>
     </nav>
   )
 }
